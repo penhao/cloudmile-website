679e138c6d4515def354fa996edb5f67036e2a45
NerdPen <83874226+NerdPen@users.noreply.github.com>
2021-05-19 11:49:13 +0800

    [Remove] MultiCDN頁面

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
-const {locales, defaultLocale} = require('./i18n.json');
+const { locales, defaultLocale } = require("./i18n.json");
 module.exports = {
     i18n: {
         locales,
         defaultLocale,
-    }
+    },
 };
-
--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,8 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
 import next from "next";
-import {redirects} from "./redirect";
+import { redirects } from "./redirect";
 const dev = process.env.NODE_ENV !== "production";
-const app = next({dev});
+const app = next({ dev });
 const handle = app.getRequestHandler();
 const port = process.env.PORT || 3000;
 
@@ -10,17 +10,19 @@ const port = process.env.PORT || 3000;
     try {
         await app.prepare();
         const server = express();
-        redirects.forEach(({from, to}) => {
+        redirects.forEach(({ from, to }) => {
             server.get(from, (req, res) => {
                 res.redirect(301, to);
-            })
+            });
         });
         server.all("*", (req: Request, res: Response) => {
             return handle(req, res);
         });
         server.listen(port, (err?: any) => {
             if (err) throw err;
-            console.log(`> Ready on server:${port} - env ${process.env.NODE_ENV}`);
+            console.log(
+                `> Ready on server:${port} - env ${process.env.NODE_ENV}`
+            );
         });
     } catch (e) {
         console.error(e);
